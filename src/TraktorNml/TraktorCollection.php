651c8e268c2c5aec8afa8faaf80ed1bcf2f4f223
Jae Task <jae.task@task-cloud.com>
2013-01-22 19:59:18 +0000

    Code coverage for traktor collection

--- src/TraktorNml/TraktorCollection.php
+++ src/TraktorNml/TraktorCollection.php
@@ -5,36 +5,72 @@ namespace TraktorNml;
 class TraktorCollection 
 {
 
+	/**
+	 * @var int version
+	 */
 	protected $version;
-	    	
+	
+	/**
+	 * Get Version
+	 * 
+	 * @return int Version
+	 */
 	public function getVersion()
 	{
 		return $this->version;
 	}
 	
+	/**
+	 * Set Version
+	 *
+	 * @param int $value value of Version to set
+	 * @return void
+	 */
 	public function setVersion($value)
 	{
 		$this->version = (int) $value;
 	}
 
-	//-- entries
+	
 	protected $entries = array();
 	
+	/**
+	 * Count number of items in Entries
+	 * 
+	 * @return int number of Entries
+	 */
 	public function numEntries()
 	{
 		return count($this->entries);
 	}
 	
+	/**
+	 * Get Entries
+	 * 
+	 * @return array Entries
+	 */
 	public function getEntries()
 	{	
 		return $this->entries;
 	}
 	
+	/**
+	 * Set Entries
+	 *
+	 * @param array $entries an array of Entries to set
+	 *
+	 * @return void
+	 */
 	public function setEntries($entries)
 	{
 		$this->entries = $entries;
 	}
 	
+	/**
+	 * Add an Entries item
+	 * 
+	 * @return void
+	 */
 	public function addEntry($entry, $key=null)
 	{
 		if ($key !== null) {
@@ -42,8 +78,7 @@ class TraktorCollection
 		} else {
 			$this->entries[] = $entry;
 		}
-	}
-	//-- entries end 
+	} 
 
 	private function initValues()
 	{
