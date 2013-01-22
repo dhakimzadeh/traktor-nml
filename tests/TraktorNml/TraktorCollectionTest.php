9745128a9904efe73aa355ea964e930df7654b6d
Jae Task <jae.task@task-cloud.com>
2013-01-22 19:58:54 +0000

    Code Coverage for TraktorCollection

--- tests/TraktorNml/TraktorCollectionTest.php
+++ tests/TraktorNml/TraktorCollectionTest.php
@@ -8,9 +8,33 @@ use \TraktorNml\TraktorCollection;
  */
 class TraktorCollectionTest extends PHPUnit_Framework_TestCase
 {
-	public function testTodo() 
+	public function testGetSetVersion() 
 	{
-		$this->assertTrue(true);
+		$mock = $this->getMock('TraktorCollection', array('getVersion','setVersion'));
+		$mock->expects($this->once())
+			->method('getVersion')
+			->will($this->returnValue(99));
+		$mock->expects($this->once())
+			->method('setVersion');
+
+		$mock->setVersion(99);
+		$this->assertEquals(99, $mock->getVersion());
+	}
+
+	public function testAddCountEntries() 
+	{
+		$mock = $this->getMock('TraktorCollection', array('addEntry','countEntries'));
+		$mock->expects($this->exactly(2))
+			->method('addEntry');
+		$mock->expects($this->exactly(3))
+			->method('countEntries')
+			->will($this->onConsecutiveCalls(0,1,2));
+
+		$this->assertEquals(0, $mock->countEntries());
+		$mock->addEntry(99);
+		$this->assertEquals(1, $mock->countEntries());
+		$mock->addEntry(99);
+		$this->assertEquals(2, $mock->countEntries());
 	}
 
 }
